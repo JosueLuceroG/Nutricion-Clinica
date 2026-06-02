8033b7ab8350cfbfb62123b53cc2fd8516b1329a
NutriClinica Dev <dev@nutriclinica.local>
2026-06-02 14:32:49 -0700

    fix(smae): mark Food as type-only import in test

--- src/modules/smae/application/smaeUseCases.test.ts
+++ src/modules/smae/application/smaeUseCases.test.ts
@@ -10,7 +10,7 @@ import {
 } from "./smaeUseCases";
 import { parseKeywordsInput, SmaeCustomFoodFormSchema } from "./smaeFormSchema";
 import {
-  Food,
+  type Food,
   type FoodId,
   type FoodRepository,
   FoodNotFoundError,
